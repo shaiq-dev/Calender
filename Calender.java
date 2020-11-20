949fe4c99997b4f2577930f234b78394f6b6eb87
Shaiq Kar <shaiqkar@gmail.com>
2020-11-20 15:12:18 +0530

    Java Calander

---
+++ Calender.java
@@ -0,0 +1,125 @@
+package Calender;
+
+import java.text.DateFormatSymbols;
+import java.time.YearMonth;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Calender {
+
+    private final List<String> Months = Arrays.asList(new DateFormatSymbols().getMonths());
+    private final List<String> Days = Arrays.asList(new DateFormatSymbols().getShortWeekdays());
+    private final String GAP = "   ";
+
+    private boolean isValidMonth(int month) {
+        if (month <= 0 || month > 12)
+            throw new InvalidMonthException();
+        return true;
+    }
+
+    private boolean isValidYear(int year) {
+        if (year <= 0)
+            throw new InvalidYearException();
+        return true;
+    }
+
+    public boolean isLeapYear(int year) {
+        if (isValidYear(year)) {
+            return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+        }
+        return false;
+    }
+
+    public int getNoOfDays(int year, int month) {
+
+        final List<Integer> _days30 = Arrays.asList(4,6,9,11);
+        final List<Integer> _days31 = Arrays.asList(1,3,5,7,8,10,12);
+
+        if (isValidYear(year) && isValidMonth(month)) {
+            if (month == 2)
+                return isLeapYear(year) ? 29 : 28;
+            if (_days30.contains(month))
+                return 30;
+            if (_days31.contains(month))
+                return 31;
+        }
+
+        return -1;
+    }
+
+    public String dayOfMonth(int year, int month, int date) {
+        if (isValidYear(year) && isValidMonth(month)) {
+            YearMonth yobj = YearMonth.of(year, month);
+            String day =  yobj.atDay(date).getDayOfWeek().name();
+            return day.charAt(0)+day.substring(1).toLowerCase();
+        }
+        return null;
+    }
+
+    public void showMonth(int year, int month) {
+
+        if (isValidYear(year) && isValidMonth(month)) {
+
+            int currentDay = Days.indexOf(dayOfMonth(year,month,1).substring(0,3))-1;
+            int totalDays = getNoOfDays(year,month);
+            print(Months.get(month-1)+", "+year+"\n");
+            Days.stream().map(d -> d + GAP).forEachOrdered(System.out::print);
+            print("\n");
+            int s;
+            for (s=0; s<currentDay; s++)
+                print(GAP+GAP);
+            for (int d=1; d<=totalDays; d++) {
+                print(String.format("%6s", d));
+                if (++s > 6) {
+                    s = 0;
+                    print("\n");
+                }
+            }
+            if (s > 0) print("\n");
+        }
+    }
+
+    public void showYear(int year) {
+        if (isValidYear(year)) {
+            print("Calender "+year+"\n");
+            for (int i=1; i<=12; i++) {
+                showMonth(year,i);
+            }
+        }
+    }
+
+    public int getCurrentYear() {
+        return YearMonth.now().getYear();
+    }
+
+    public int getCurrentMonthIndex() {
+        return YearMonth.now().getMonthValue();
+    }
+
+    public String getCurrentMonth() {
+        return Months.get(getCurrentMonthIndex() - 1);
+    }
+
+
+//    Utility Methods
+    private void print(Object s) {
+        System.out.print(s);
+    }
+
+}
+
+// Exceptions
+class InvalidMonthException extends RuntimeException {
+    public InvalidMonthException() {
+        super("Invalid Month, Month index should lie between 1-12");
+    }
+}
+
+class InvalidYearException extends RuntimeException {
+    public InvalidYearException() {
+        super("Year Can't be negative or zero");
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,88 @@
+# Java Calender
+Java Calender is a simple commandline calender tool. 
+
+# Methods!
+To start first create an instance of the class
+```sh
+Calender cal = new Calander();
+```
+
+ # showMonth(int year, int month) 
+  It displays the calender of the given month of the given year
+  `returnType: void`
+ ```sh
+    cal.showMonth(2020,11);
+    //Output
+    November, 2020
+       Sun   Mon   Tue   Wed   Thu   Fri   Sat   
+         1     2     3     4     5     6     7
+         8     9    10    11    12    13    14
+        15    16    17    18    19    20    21
+        22    23    24    25    26    27    28
+        29    30
+```
+ # showYear(int year)
+ It displays the calender of the given year. 
+ `returnType: void`
+  ```sh
+    cal.showYear(2020);
+  ```
+# dayOfMonth(int year, int month, int date)
+It returns the name of the day on the given date. 
+`returnType: String`
+```sh
+    cal.dayOfMonth(2020,11,1);
+    // Output
+    Sunday
+```
+
+# getNoOfDays(int year, int month)
+It returns the number of days in the given month.
+`@param: year` It is required to check if the year is a leap year or not.
+`returnType: String`
+```sh
+    cal.getNoOfDays(2020,11);
+    // Output
+    30
+```
+# isLeapYear(int year)
+It returns true if the year is a leap year else false. 
+`returnType: bool`
+```sh
+    cal.isLeapYear(2020);
+    // Output
+    true
+```
+# getCurrentYear()
+It returns the integer value of the current year. 
+`returnType: int`
+```sh
+    cal.getCurrentYear();
+    // Output
+    2020
+```
+# getCurrentMonth()
+It returns the name of current month. 
+`returnType: String`
+```sh
+    cal.getCurrentMonth();
+    // Output
+    November
+```
+# getCurrentMonthIndex()
+It returns the integer value of current month. 
+`returnType: int`
+`1 - January ... 12 - December`
+```sh
+    cal.getCurrentMonthIndex();
+    // Output
+    11
+```
+
+License
+----
+
+MIT
+
+
+ 
